a28f0e68a172e71c6af8fe7d2e10e87568855e85
887 <2300887@gmail.com>
2024-06-06 00:01:14 +0200

    subscribe to logs on thread

--- src/main.rs
+++ src/main.rs
@@ -57,9 +57,16 @@ async fn main() -> std::io::Result<()> {
     let block_reloads_mutex_task = block_reloads_mutex.clone();
     let server_is_running_reader = server_is_running.clone();
 
-    #[cfg(feature = "log")]
-    let log_subscription_observe = get_log_subscription()?;
     tokio::task::spawn(async move {
+        #[cfg(feature = "log")]
+        let log_subscription_observe = match get_log_subscription() {
+            Ok(ls) => ls,
+            Err(_err) => {
+                tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+                return;
+            }
+        };
+        trace!("test");
         let res = observe::run(
             server_is_running_reader,
             tx_shutdown_server,
