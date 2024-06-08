9eda5222dadf384680ffc6da98cf65142c591646
887 <2300887@gmail.com>
2024-06-08 23:15:57 +0200

    license, update script

--- update_backend.sh
+++ update_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cargo install cargo-progenitor
-cargo progenitor -i api/swagger.json -o backend -n backend -v 0.1.0
+cargo cargo progenitor -i api/swagger.json -o backend -n backend -v 0.1.0 --include-client true
