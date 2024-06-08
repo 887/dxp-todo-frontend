1dc391a2dd0bc02117b14a2a3c8f02ebf17573da
887 <2300887@gmail.com>
2024-06-08 21:31:53 +0200

    index session used

--- server/src/endpoints/index.rs
+++ server/src/endpoints/index.rs
@@ -1,8 +1,24 @@
-use poem::handler;
+use poem::{handler, session::Session};
 use tracing::trace;
 
+pub static SESSION_INDEX_COUNTER: &str = "index_counter";
+
 #[handler]
-pub fn index() -> String {
+pub fn index(session: &Session) -> String {
+    let counter = session
+        .get::<usize>(SESSION_INDEX_COUNTER)
+        .map_or(0, |v| v + 1);
+
+    session.set(SESSION_INDEX_COUNTER, counter);
+
+    let hello = format!("hello world! {}", counter);
+    trace!("{}", &hello);
+
+    hello.to_owned()
+}
+
+#[handler]
+pub fn index2() -> String {
     let hello = "hello world!".to_string();
     trace!("{}", &hello);
 
