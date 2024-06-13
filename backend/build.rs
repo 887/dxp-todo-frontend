786d02ab7834aaa954bcb9e2fe84e97e59108334
887 <2300887@gmail.com>
2024-06-14 01:42:54 +0200

    swagger.json in backend dir

--- backend/build.rs
+++ backend/build.rs
@@ -1,6 +1,6 @@
 //https://github.com/oxidecomputer/progenitor?tab=readme-ov-file#buildrs
 fn main() {
-    let src = "../api/swagger.json";
+    let src = "swagger.json";
     println!("cargo:rerun-if-changed={}", src);
     let file = std::fs::File::open(src).unwrap();
     let spec = serde_json::from_reader(file).unwrap();
