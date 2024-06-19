c64bc7bf6b81e3ace57ded696127c3691725220d
887 <2300887@gmail.com>
2024-06-19 02:17:42 +0200

    github only disable prebuild

--- server/build.rs
+++ server/build.rs
@@ -1,14 +1,18 @@
+#[cfg(not(feature = "github"))]
 use std::process::Command;
 
 fn main() {
+    #[cfg(not(feature = "github"))]
     let current_manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap().to_string();
+    #[cfg(not(feature = "github"))]
     let css_builder = current_manifest.clone() + "/tools/css-builder";
+    #[cfg(not(feature = "github"))]
     let css_builder_manifest = css_builder.clone() + "/Cargo.toml";
 
     println!("cargo:rerun-if-changed={}", css_builder_manifest);
 
-    //only build this when developing this way, otherwise use build script
-    #[cfg(debug_assertions)]
+    //with github do not run this pre-build command, otherwise the github action hangs!
+    #[cfg(not(feature = "github"))]
     Command::new("cargo")
         .arg("build")
         .arg("--release")
