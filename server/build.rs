620b252f3a532bd3bc0b0a654bb49d6a6fb06804
887 <2300887@gmail.com>
2024-06-19 02:48:01 +0200

    no escargot needed

--- server/build.rs
+++ server/build.rs
@@ -9,6 +9,7 @@ fn main() {
     #[cfg(not(feature = "github"))]
     let css_builder_manifest = css_builder.clone() + "/Cargo.toml";
 
+    //this line means we only trigger a rebuild if the manifest of the submodule changes
     #[cfg(not(feature = "github"))]
     println!("cargo:rerun-if-changed={}", css_builder_manifest);
 
@@ -20,24 +21,4 @@ fn main() {
         .current_dir(css_builder)
         .spawn()
         .expect("cargo build command failed to start");
-
-    // let tools = current_manifest.clone() + "/tools/out";
-    // let tools = std::path::Path::new(&tools);
-    // std::fs::create_dir(tools);
-    // let run = escargot::CargoBuild::new()
-    //     .current_target()
-    //     .manifest_path(css_builder_manifest)
-    //     .target_dir(tools)
-    //     .exec()
-    //     .unwrap();
-
-    // let result = current_manifest.clone() + "/tools/result";
-    // std::fs::write(result.clone(), format!("{}", ""));
-    // let mut f = std::fs::File::options().append(true).open(result).unwrap();
-    // for m in run {
-    //     match m {
-    //         Ok(m) => writeln!(&mut f, "{}", m.).unwrap(),
-    //         Err(ce) => writeln!(&mut f, "").unwrap(),
-    //     }
-    // }
 }
