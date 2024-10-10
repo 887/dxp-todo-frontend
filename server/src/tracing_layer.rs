7ec55bd3b6e3b68d0678560610160f3b0932a93d
887 <2300887@gmail.com>
2024-10-10 03:46:32 +0200

    tracing layer

--- server/src/tracing_layer.rs
+++ server/src/tracing_layer.rs
@@ -1,26 +1,33 @@
 // This should always be the last layer in the middleware stack.
 // Otherwise trace logging will not be complete.
 
-use axum::{body::Body, extract::Request, response::Response};
+use axum::{extract::Request, response::Response};
 use std::task::{Context, Poll};
 use tower::{Layer, Service};
 
 type BoxFuture<'a, T> = std::pin::Pin<Box<dyn std::future::Future<Output = T> + Send + 'a>>;
 
 #[derive(Clone)]
-pub struct TracingLayer;
+pub struct TracingLayer {
+    pub log_dispatcher: dxp_logging::LogDispatcher,
+}
 
 impl<S> Layer<S> for TracingLayer {
     type Service = TracingMiddleware<S>;
 
     fn layer(&self, inner: S) -> Self::Service {
-        TracingMiddleware { inner }
+        let log_dispatcher = self.log_dispatcher.clone();
+        TracingMiddleware {
+            inner,
+            log_dispatcher,
+        }
     }
 }
 
 #[derive(Clone)]
 pub struct TracingMiddleware<S> {
     inner: S,
+    log_dispatcher: dxp_logging::LogDispatcher,
 }
 
 impl<S> Service<Request> for TracingMiddleware<S>
@@ -39,19 +46,15 @@ where
 
     fn call(&mut self, request: Request) -> Self::Future {
         let future = self.inner.call(request);
-        Box::pin(async move {
-            match dxp_logging::get_subscriber() {
-                Ok(subscriber) => {
-                    let log_subscription = dxp_logging::get_subscription_for_subscriber(subscriber);
-                    let response: Response = future.await?;
-                    drop(log_subscription);
-                    Ok(response)
-                }
-                Err(err) => {
-                    let response = Response::new(Body::from(err.to_string()));
-                    Ok(response)
-                }
-            }
-        })
+        let log_dispatcher = self.log_dispatcher.clone();
+        let log_guard = dxp_logging::set_thread_default_dispatcher(&log_dispatcher);
+        let res = Box::pin(async move {
+            let log_guard = dxp_logging::set_thread_default_dispatcher(&log_dispatcher);
+            let response: Response = future.await?;
+            drop(log_guard);
+            Ok(response)
+        });
+        drop(log_guard);
+        res
     }
 }
