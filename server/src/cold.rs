056bd5c303ab06c8765fdfc2844ccf12b6fa4c3a
887 <2300887@gmail.com>
2024-10-11 02:17:34 +0200

    cold update

--- server/src/cold.rs
+++ server/src/cold.rs
@@ -7,18 +7,22 @@ pub extern "Rust" fn load_env() -> Result<std::path::PathBuf> {
     Ok(dotenvy::dotenv().map_err(|_| "could not load .env")?)
 }
 
+#[cfg(feature = "log")]
 pub extern "Rust" fn run_server() -> Result<()> {
-    #[cfg(feature = "log")]
     let log_dispatcher = dxp_logging::get_subscriber()
         .map_err(|e| anyhow::anyhow!("could not get log subscriber: {}", e))?
         .get_dispatcher();
-    #[cfg(feature = "log")]
     let log_guard = dxp_logging::set_thread_default_dispatcher(&log_dispatcher);
 
     let empty = None::<Option<()>>.map(|_| async {});
     let res = Ok(run_server_main(empty, &log_dispatcher)?);
 
-    #[cfg(feature = "log")]
     drop(log_guard);
     res
+}
+
+#[cfg(not(feature = "log"))]
+pub extern "Rust" fn run_server() -> Result<()> {
+    let empty = None::<Option<()>>.map(|_| async {});
+    run_server_main(empty)
 }
